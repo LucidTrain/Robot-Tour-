e17e648e28a905137537a4384cc33278a6b11970
Li Sun <sufam5@outlook.com>
2025-01-10 13:15:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from microdot import Microdot, Response
 import ujson
 from execute import boot2
 import machine
-
+import neopixel
+import time
 def read_webpage():
     with open('index.html', 'r') as file:
         return file.read()
@@ -16,24 +17,52 @@ def write_commands(data):
     with open ('commands.json', 'w') as file3:
         ujson.dump(data, file3)
         return True
-runcheck = read_commands()
-if runcheck['launch'] == 'True':
-    boot2()
-    machine.reset()
+try:
+    runcheck = read_commands()
+    if runcheck['launch'] == 'True':
+        try:
+            boot2()
+        except Exception:
+            np = neopixel.NeoPixel(machine.Pin(48), 1)
+            while True:
+                np[0] = (128,0,128)
+                np.write()
+                time.sleep(0.25)
+                np[0] = (0,0,0)
+                np.write()
+                np[0] = (128,0,128)
+                np.write()
+                time.sleep(0.25)
+                np[0] = (0,0,0)
+                np.write()
+                np[0] = (128,0,128)
+                np.write()
+                time.sleep(0.25)
+                np[0] = (0,0,0)
+                np.write()
+        machine.reset()
+except Exception:
+    pass
 ap = network.WLAN(network.AP_IF)
+ap.config(essid='CheapChip', password='djjdm!334')
+ap.config(authmode=network.AUTH_WPA2_PSK)
 ap.active(True)
-ap.config(essid='SETUP-D8J83', password='54142424145415241201414114141')
+
+
 app = Microdot()
 
 
 @app.route('/')
 def index(request):
-    data = "html read from file"
-    return Response(body=data, contentmanager='text/html')
+    with open('index.html', 'r') as file4:
+        data = file4.read()
+   
+    return Response(body=data, status_code=200, headers={'Content-Type': 'text/html'})
 @app.route('/submit', methods=['POST'])
 def handle_post(request):
     data = request.form.get('data', 'No data received')
     print(f"Received POST data: {data}")  # Log the data
     return {'status': 'success', 'data': data}
 app.run(host="0.0.0.0", port=80)
+
 
